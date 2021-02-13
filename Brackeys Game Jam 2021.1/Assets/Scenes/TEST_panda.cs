936499eb17247dd2ea6c429bca82775227b2c91e
Panda <dev.pandabear@gmail.com>
2021-02-14 02:03:40 +0530

    added empty gameobject "Test", and a script which just prints editor and application version

---
+++ Brackeys Game Jam 2021.1/Assets/Scenes/TEST_panda.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_panda : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Editor Version " + Application.unityVersion + '\n' + "Project Version " + Application.version);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
