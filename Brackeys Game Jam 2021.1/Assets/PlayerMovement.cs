7a3d89dd929d216a507579784c7a3c0357919fbb
SeprAYY <julian.labanowski@wp.pl>
2021-02-14 14:01:43 +0100

    Basic movement

---
+++ Brackeys Game Jam 2021.1/Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    float moveInput;
+    bool facingRight = true;
+
+    [SerializeField] int extraJumps;
+    int jumpCounter;
+
+    [SerializeField] LayerMask whatIsGround;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius;
+    bool isGrounded;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(isGrounded)
+        {
+            jumpCounter = extraJumps;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && jumpCounter > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jumpCounter--;
+        }
+
+        moveInput = Input.GetAxis("Horizontal");
+
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        if(facingRight)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0f, transform.eulerAngles.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180f, transform.eulerAngles.z);
+        }
+    }
+}
