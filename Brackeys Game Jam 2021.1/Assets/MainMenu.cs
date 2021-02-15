5cc473438e5bc57e81e46ea441687744f685d94a
Zequals <okclave@gmail.com>
2021-02-15 21:19:34 +0000

    FMOD volume mixer settings

--- Brackeys Game Jam 2021.1/Assets/MainMenu.cs
+++ Brackeys Game Jam 2021.1/Assets/MainMenu.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    
+    FMOD.Studio.Bus busMaster;
+    FMOD.Studio.Bus busSFX;
+    FMOD.Studio.Bus busMusic;
+
+    
+
+    private void Start()
+    {
+        busMaster = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+        busSFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        busMusic = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+    }
+
     public void SetMasterVolume(float volume)
     {
+        busMaster.setVolume(volume);
         Debug.Log("Setting Master volume to " + volume);
     }
 
     public void SetSFXVolume(float volume)
     {
+        busSFX.setVolume(volume);
         Debug.Log("Setting SFX volume to " + volume);
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/sfx_volume", transform.position);
     }
 
     public void SetMusicVolume(float volume)
     {
-        Debug.Log("Setting Music volume to " + volume);
+        busMusic.setVolume(volume);
+        Debug.Log("Setting Music volume to " + volume); 
     }
 }
