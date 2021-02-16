52b8b6f8edb8b838e1d2886c47fe1573cf7ee1f6
MatherPL <61986656+MatherPL@users.noreply.github.com>
2021-02-16 21:04:35 +0100

    CameraFix

---
+++ Brackeys Game Jam 2021.1/Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject vCam;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")&&!collision.isTrigger)
+        {
+            vCam.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            vCam.SetActive(false);
+        }
+    }
+}
